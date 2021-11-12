2da735cd4efdb9eefb975df01c002da9508cda9f
E-Molostova <molostova8973@gmail.com>
2021-11-12 15:07:57 +0200

    added styles

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,29 +4,32 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { mainRoutes } from '../../routes/mainRoutes';
 import MovieDetailsPage from '../../pages/MovieDetailsPage';
 import Footer from '../Footer/Footer';
+import Container from '../Container/Container';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Suspense fallback={<h2>Loading...</h2>}>
-          <Switch>
-            <Route path="/movie/:movieId">{MovieDetailsPage}</Route>
-            {mainRoutes.map(route => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={route.path}
-                component={route.component}
-              />
-            ))}
-            <Route render={() => <h1>Page not found</h1>} path={'/not-found'} />
-            <Redirect to="/" />
-          </Switch>
-        </Suspense>
-      </main>
-      <Footer />
+      <Container>
+        <Header />
+        <main>
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <Switch>
+              <Route path="/movie/:movieId">{MovieDetailsPage}</Route>
+              {mainRoutes.map(route => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={route.path}
+                  component={route.component}
+                />
+              ))}
+              <Route render={() => <h1>Page not found</h1>} path={'/not-found'} />
+              <Redirect to="/" />
+            </Switch>
+          </Suspense>
+        </main>
+        <Footer />
+      </Container>
     </>
   );
 };
